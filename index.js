c7db3135a24f3a5930855f05bee5f54f6ed6e7c1
Berkay Bideci <berkaybideci@gmail.com>
2023-05-04 22:07:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+        .then(response => response.json())
+        .then(userData => {
+            const div = document.createElement("div")
+            div.textContent = userData.id
+            document.body.appendChild(div)
+        })
+        .catch(userDataError => {
+            const div = document.createElement("div")
+            div.textContent = userDataError
+            document.body.appendChild(div)
+        })
+}
+
+submitData("Steve", "steve@steve.com")
